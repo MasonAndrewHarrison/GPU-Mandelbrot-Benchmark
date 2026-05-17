f7c58eeab062ee4abf6120dc1230b82e6100152a
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2026-05-17 18:59:41 -0400

    add camera control and ball position tracking.

--- main.c
+++ main.c
@@ -9,27 +9,30 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - input keys");
+    InitWindow(screenWidth, screenHeight, "Raylib");
 
-    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    Camera2D camera = { 0 };
+    camera.offset = (Vector2){ screenHeight/2.0f, screenWidth/2.0f };
+    Vector2 ballPosition = {screenHeight/2.0f, screenHeight/2.0f};
+    camera.zoom = 1.0f;
+
 
     SetTargetFPS(60);       
 
     while (!WindowShouldClose())   
     {
 
-        if (IsKeyDown(KEY_D)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_A)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_W)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_S)) ballPosition.y += 2.0f;
+        if (IsKeyDown(KEY_D)) camera.offset.x += 2.0f;
+        if (IsKeyDown(KEY_A)) camera.offset.x -= 2.0f;
+        if (IsKeyDown(KEY_W)) camera.offset.y -= 2.0f;
+        if (IsKeyDown(KEY_S)) camera.offset.y += 2.0f;
+
+        ballPosition = GetMousePosition();
 
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
-
-            DrawCircleV(ballPosition, 50, MAROON);
+        ClearBackground(RAYWHITE);
+        DrawCircleV(ballPosition, 50, MAROON);
 
         EndDrawing();
     }
